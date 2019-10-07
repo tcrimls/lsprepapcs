2b56cd0895eeadae141b88ae50ef1e187ad24a06
tcrimls <55150745+tcrimls@users.noreply.github.com>
2019-10-07 09:47:09 -0700

    Add files via upload

--- Unit1Project/main.java
+++ Unit1Project/main.java
@@ -1,14 +1,20 @@
-//this is nation simulator. the game of being a country//
+//this is Nation simulator. the game of being a country//
 
 import java.util.*;
 import java.lang.*;
+
+//each city gives 100 industry strength and culture//
+//boosting imporves that for one city//
 
 //turn structure
 //1 world report
 //2 country event
 //3 actions
 
-public class main extends nation{
+//actions
+//boost - for each 100 you spend, you can boost strength, industry, or culture//
+
+public class main extends Nation{
   static boolean gameon = true;//this is to check if the game should keep running
   static void print(String inp){
     //make it more like python
@@ -36,9 +42,9 @@ public class main extends nation{
     }
   }
 
-  static nation psetup(){
+  static Nation psetup(){
     Scanner keyboard = new Scanner(System.in);
-    nation Player = new nation(); //object for the player's country
+    Nation Player = new Nation(); //object for the player's country
 
     print("---------------------------------\n");
     print("\n");
@@ -59,7 +65,7 @@ public class main extends nation{
     print("And what is your name?\n");
     String pname = keyboard.nextLine();
     print("\n");
-    print("All hail "+pname+", king of the "+cont+" country "+cname);
+    print("All hail "+pname+", king of the "+cont+" country "+cname+"\n");
 
     Player.name = cname;
     Player.cont = cont;
@@ -68,61 +74,101 @@ public class main extends nation{
     return Player;
   }
 
-  static nation [] gsetup(nation Player){
+  static Nation [] gsetup(Nation Player){
     //define all of the information about countries you deal with
-    nation Jamaica = new nation();
+    Nation Jamaica = new Nation();
     Jamaica.name = "Jamaica";
     Jamaica.cont = "America";
     Jamaica.king = "Norman Manely";
-    nation Zaire = new nation();
+    Nation Zaire = new Nation();
     Zaire.name = "Zaire";
     Zaire.cont = "Africa";
     Zaire.king = "Robert Mugabe";
-    nation Burma = new nation();
+    Nation Burma = new Nation();
     Burma.name = "Burma";
     Burma.cont = "Asia";
     Burma.king = "Than Shwe";
-    nation [] nations = {Jamaica,Zaire,Burma,Player};
-    return nations;
+    Nation [] Nations = {Jamaica,Zaire,Burma,Player};
+    return Nations;
   }
 
-  static nation report(nation nations){
-    nation Player = nations[3];
+  static void report(Nation [] Nations){
+    Nation Player = Nations[3];
     //first part of a turn. just gives surface level info about other countries
     print("---------------------------------\n");
-    print(Player.name+"'s Turn!")
+    print(Player.name+"'s Turn!\n");
     print("---------------------------------\n");
     print("\n");
     print("---------------------------------\n");
     print("World Report:\n");
-    //check to see if different nations qualify for info
+    //check to see if different Nations qualify for info
     for(int i = 0; i<4;i++){
       //economic info
-      if (nations[i].money<=0){
-        print("-"+nation[i].name+" is broke!")
+      if (Nations[i].money<=0){
+        print("-"+Nations[i].name+" is broke!\n");
       }
-      else if (nations[i].money<=10000){
-        print("-"+nation[i].name+"'s coffers are running low.")
+      else if (Nations[i].money<=10000){
+        print("-"+Nations[i].name+"'s coffers are running low.\n");
       }
-      else if (nation[i].money<500000){
-        print("-"+nation[i].name+" is Rich!")
+      else if (Nations[i].money>500000){
+        print("-"+Nations[i].name+" is Rich!\n");
       }
-    }
-  }
 
-  static void turn(nations){
+      //industrial info
+      if (Nations[i].industry>200){
+        print("-"+Nations[i].name+" is getting industrial.\n");
+      }
+      else if (Nations[i].industry>350){
+        print("-"+Nations[i].name+" is super industrial.\n");
+      }
+      else if (Nations[i].industry>500){
+        print("-"+Nations[i].name+" is an industrial powerhouse!\n");
+      }
+
+      //culture info
+      if (Nations[i].culture>200){
+        print("-"+Nations[i].name+" is culturally progressing.\n");
+      }
+      else if (Nations[i].culture>350){
+        print("-"+Nations[i].name+" has a thriving culture.\n");
+      }
+      else if (Nations[i].culture>500){
+        print("-"+Nations[i].name+" is a cultural hub!\n");
+      }
+
+      //strength info
+      if (Nations[i].strength<50){
+        print("-"+Nations[i].name+" is ripe for the taking!\n");
+      }
+      if (Nations[i].strength>200){
+        print("-"+Nations[i].name+" is getting stronger.\n");
+      }
+      else if (Nations[i].strength>200){
+        print("-"+Nations[i].name+" is a forbidable power.\n");
+      }
+      else if (Nations[i].strength>200){
+        print("-"+Nations[i].name+" is an unstoppable force of nature!\n");
+      }
+
+    }//close for loop
+    print("---------------------------------\n");
+  }//close world report
+
+  static void turn(Nation [] Nations){
     //manage turn order
-    report(nations);
+    report(Nations);
   }
 
   public static void main(String[] args){
     //manage gameplay order
     intro();
-    nation Player = psetup();
-    nation [] nations = gsetup(Player);
-    while (gameon){
-      turn(nations);
+    Nation Player = psetup();
+    Nation[] Nations = gsetup(Player);
+    Nations[0].money = 600000;
+    turn(Nations);
+    //while (gameon){
+      //turn(Nations);
 
-    }//turn
+    //turn
   }//Close main
 }//close class
